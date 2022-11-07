800b8582ee898b0e04d5b3be719c43f3942d83dd
alexey <asivlev@yandex.ru>
2022-11-08 00:30:51 +0300

    The product of numbers

---
+++ Seminar4_ProdNums/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 28. Напишите программу, которая принимает на вход число N 
+// и выдает произведение чисел от 1 до N. Например, 4 -> 24, 5 -> 120.
+Console.Clear();
+Console.WriteLine("Уважаемый пользователь, введите чило: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел от 1 до {number} = {ProdNums(number)}.");
+
+int ProdNums(int number)             // Опишем метод.
+{
+    int result = 1;                  // Присвоим переменной значение 1. 
+    while(number > 1)                // Зададим условие выхода из цикла. 
+    {
+        result = result*number;      // Расчитаем произведение чисел от 1 до N. 
+        number--;                    // Введем декремент. 
+        Console.Write($"{result} "); // Выведем на печать промежуточные значения. 
+    }
+    Console.WriteLine();             // Перейдем на новую строчку.
+    return result;                   // Вернем результат.
+}

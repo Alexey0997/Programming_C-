de18f36ec00d217a01892e26433e86e5eccba232
alexey <asivlev@yandex.ru>
2022-11-12 16:12:05 +0300

    Add Min and Max methods in the program

--- S5_Homework_MinMaxDif_InArray/Program.cs
+++ S5_Homework_MinMaxDif_InArray/Program.cs
@@ -6,23 +6,9 @@ Console.Clear();                         // Отчистка консоли.
 double[] array = new double[8];          // Инициализация массива вещественных чисел.
 FillArray(array);                        // Вызов метода заполнения массива. 
 PrintArray(array);                       // Вызов метода печати массива. 
-double min = array[0];                   // Ввод переменной минимального значения.
-for (int i = 0; i < array.Length; i++)   // Цикл исследования элементов массива. 
-{
-    if (min > array[i]) min = array[i];  // Определение минимального элемента. 
-}
-Console.WriteLine();                     // Печать минимального значения с новой строки. 
-Console.Write($"Минимальный элемент массива = {min}.");
-
-double max = array[0];                   // Ввод переменной максимального значения. 
-for (int i = 0; i < array.Length; i++)   // Цикл исследования массива. 
-{
-    if (max < array[i]) max = array[i];  // Определение максимального элемента. 
-}
-Console.WriteLine();                     // Печать максимального значения с новой строки. 
-Console.Write($"Максимальный элемент массива = {max}.");
-Console.WriteLine();                     // Вывод на печать разницы между мин. и макс. значениями. 
-Console.Write($"Разница между максимальным и минимальным элементами массива = {max - min}.");
+double res = MaxArray(array) - MinArray(array); // Вычисление разницы между мин. и макс. значениями. 
+Console.WriteLine();                     // Вывод на печать результата. 
+Console.Write($"Разница между максимальным и минимальным элементами массива = {res}.");
 
 // МЕТОДЫ (ПОДПРОГРАММЫ)
 void FillArray(double[] array)             // Метод заполнения массива. 
@@ -45,40 +31,26 @@ void PrintArray(double[] array)            // Метод вывода масси
     Console.Write("]");                    // Закрывающая квадратная скобка. 
 }
 
+double MinArray(double[] array)            // Метод определения минимального эл-та массива. 
+{
+    double min = array[0];                 // Ввод переменной для обозначения мин. 
+    for (int i = 0; i < array.Length; i++) // Цикл исследования элементов массива. 
+    {
+        if (min > array[i]) min = array[i];// Определение минимального элемента. 
+    }
+    Console.WriteLine();                   // Печать минимального значения с новой строки. 
+    Console.Write($"Минимальный элемент массива = {min}.");
+    return min;                            // Возврат мин. в основную программу. 
+}
 
-// Задача 38: Задайте массив вещественных чисел. 
-// Найдите разницу между максимальным и минимальным элементами массива.
-// Например, [3 7 22 2 78] -> 76.
-// Console.Clear();
-// int[] array = GetArray(10, 1, 100);
-// int minNumber = array[0];
-
-// for (int i = 0; i < array.Length; i++)
-// {
-//     if(minNumber > array[i]) minNumber = array[i];
-// }
-// Console.WriteLine();
-// Console.Write($"Минимальный элемент массива = {minNumber}.");
-
-// int maxNumber = array[0];
-// for (int i = 0; i < array.Length; i++)
-// {
-//     if(maxNumber < array[i]) maxNumber = array[i];
-// }
-// Console.WriteLine();
-// Console.Write($"Максимальный элемент массива = {maxNumber}.");
-// int res = maxNumber - minNumber;
-// Console.WriteLine();
-// Console.Write($"Разница между максимальным и минимальным элементами массива = {res}.");
-
-// // Метод формирования массива. 
-// int[] GetArray(int size, int minValue, int maxValue) 
-// {
-//     int[] res = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         res[i] = new Random().Next(minValue, maxValue + 1);
-//         Console.Write($"{res[i]} ");
-//     }
-//     return res;
-// }
+double MaxArray(double[] array)            // Метод для нахождения макс. элемента массива. 
+{
+    double max = array[0];                 // Ввод переменной максимального значения. 
+    for (int i = 0; i < array.Length; i++) // Цикл исследования массива. 
+    {
+        if (max < array[i]) max = array[i];// Определение максимального элемента. 
+    }
+    Console.WriteLine();                   // Печать максимального значения с новой строки. 
+    Console.Write($"Максимальный элемент массива = {max}.");
+    return max;                            // Возврат макс. в основную программу. 
+}
